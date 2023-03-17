b626a234c7f9358dd3f108d7fac5568f679c7bd7
MohdBasurra <mohdye96@gmail.com>
2023-03-17 18:57:56 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+let newMain = document.getElementById("main");
+newMain.remove();
+
+// add
+const newHeader = document.createElement("h1");
+
+
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "Mohammed Basurra is the champion";
+
+
+document.body.append(newHeader);
